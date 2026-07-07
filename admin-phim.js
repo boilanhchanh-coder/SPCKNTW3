2751d45a6e5acdc5862609fff72f7d410492fd90
boilanhchanh-coder <boilanhchanh@gmail.com>
2026-07-07 10:32:42 +0000

    okk

---
+++ admin-phim.js
@@ -0,0 +1,8 @@
+function openModal(){
+    let ok = document.querySelector(".modal");
+    ok.style.display = "flex";
+};
+function closeModal(){
+    let ok = document.querySelector(".modal");
+    ok.style.display = "none";
+}
--- data.js
+++ data.js
@@ -1,17 +1,11 @@
-document.querySelectorAll('.select-button').forEach(btn => {
-    btn.addEventListener('click', () => {
-        document.querySelectorAll('.select-button').forEach(b => b.classList.remove('active'));
-        btn.classList.add('active');
-    });
-});
-//Lệnh
+//Hàm lấy dữ liệu và lưu dữ liệu
 function getData(key) {
     return JSON.parse(localStorage.getItem(key)) || [];
 }
 function saveData(key, data) {
     localStorage.setItem(key, JSON.stringify(data));
 }
-//Tài khoản ADMIN, CUSTOMER
+//Tài khoản ADMIN
 function tkAdminCoSan(){
     let accounts = getData("accounts");
     let adminExists = accounts.some(acc => acc.username === "admin");
@@ -26,14 +20,32 @@ function tkAdminCoSan(){
     }
 }
 tkAdminCoSan();
-function tkCustomer(){
-    let accounts = getData("accounts");
+//Đăng kí
+function register(){
+    let regUser = document.getElementById('regUser').value;
+    let regPass = document.getElementById('regPass').value;
+    let confirm = document.getElementById('xac-nhan').value;
+    let accounts = getData('accounts');
+    if (!accounts.some(a => a.username === regUser) && regPass === confirm){
+        accounts.push({
+            username: regUser,
+            password: regPass,
+            role: "User"
+        });
+        saveData("accounts", accounts);
+    }
 }
 //Đăng nhập
 function login(){
-    let loginUser = document.getElementById("taikhoan").value;
-    let loginPass = document.getElementById("matkhau").value;
+    let loginUser = document.getElementById("loginUser").value;
+    let loginPass = document.getElementById("loginPass").value;
     let accounts = getData("accounts");
-    let acc = accounts.find(a.username === loginUser && a.password === loginPass);
-    
+    let acc = accounts.find(a => a.username === loginUser && a.password === loginPass);
+    saveData("NguoiDung", loginUser);
+    if (acc.role === "Admin") location.href = "admin-phim.html";
+    else location.href = "trang-chu.html"
+}
+//Đăng xuất
+function logout(){
+    location.href = "index1.html";
 }
