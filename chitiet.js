fdffa5c94f716d0a371d879080ec23d1ebc4faf7
heoki <tthuphuong1009@gmail.com>
2026-07-25 16:40:54 +0700

    dmm

---
+++ chitiet.js
@@ -0,0 +1,37 @@
+
+
+let id = new URLSearchParams(location.search).get("id");
+
+hienThiChiTiet(id);
+function hienThiChiTiet(id){
+    let danhSachPhim = getData("danhSachPhim");
+    let phim = danhSachPhim.find(p => p.id == id);
+    let box = document.getElementById("chiTiet");
+    if (phim) {
+    box.innerHTML = `
+        <div class="thong-tin-phim">
+
+            <div class="poster">
+                <img src="${phim.anh}">
+            </div>
+
+            <div class="info">
+
+                <h1>${phim.tenPhim}</h1>
+
+                <p><b>Thể loại:</b> ${phim.theLoai}</p>
+
+                <p><b>Thời lượng:</b> ${phim.thoiLuong}</p>
+                <h2>NỘI DUNG PHIM</h2>
+
+                 <p>${phim.moTa}</p>
+                 <button class="buy">MUA VÉ</button>
+            </div>
+            
+        </div>
+       
+        `;
+} else {
+    box.innerHTML = "<h2>Không tìm thấy phim!</h2>";
+}
+}
